8a621b61bbb243697388f13434997b8d31d82afa
Sofoniasabebe <abebesofonias@gmail.com>
2025-03-11 12:53:57 -0700

    Sofonias' branch completed.

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,60 @@
+-- Data Engineering
+
+DROP TABLE IF EXISTS category CASCADE;
+DROP TABLE IF EXISTS subcategory CASCADE;
+DROP TABLE IF EXISTS contacts CASCADE;
+DROP TABLE IF EXISTS campaign CASCADE; 
+
+-- 1. Category table - no dependencies
+-- For this table, category_id is used as a primary key
+
+CREATE  TABLE category (
+    category_id VARCHAR PRIMARY KEY,
+    category VARCHAR NOT NULL
+);
+
+-- 2. Subcategory table - no dependencies
+-- For this table, subcategory_id is used as a primary key
+
+CREATE TABLE subcategory (
+    subcategory_id VARCHAR PRIMARY KEY,
+    subcategory VARCHAR NOT NULL
+);
+
+-- 3. Contacts table - no dependencies
+-- For this table, contact_id is used as a primary key.
+
+CREATE TABLE contacts (
+    contact_id INT PRIMARY KEY,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    email VARCHAR NOT NULL
+);
+
+-- 4. The campaign table references category, subcategory, and contacts. 
+
+CREATE TABLE campaign (
+    cf_id INT PRIMARY KEY,
+    contact_id INT,
+    comapny_name VARCHAR NOT NULL,
+    description VARCHAR,
+    goal FLOAT,
+    pledged FLOAT,
+    outcome VARCHAR,
+    backers_count INT,
+    country VARCHAR,
+    currency VARCHAR,
+    launch_date DATE,
+    end_date DATE,
+    category_id VARCHAR,
+    subcategory_id VARCHAR,
+    FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
+    FOREIGN KEY (category_id) REFERENCES category(category_id),
+    FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id)
+);
+
+-- Viewing the tables:
+SELECT * FROM category;
+SELECT * FROM subcategory;
+SELECT * FROM contacts;
+SELECT * FROM campaigns;
